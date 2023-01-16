d43b7af1578438f49c1b4546b95e05f8e4a5fd4c
Justin Shrake <justinshrake@gmail.com>
2023-01-16 06:41:30 -0800

    Cleanup and factor the chart into a separate component

--- src/lib/Output.svelte
+++ src/lib/Output.svelte
@@ -1,28 +1,14 @@
 <script lang="ts">
-  import "charts.css";
-  import type HypergeometricParameters from "./HypergeometricParameters";
-  import hypergeometric_cdf from "@stdlib/stats-base-dists-hypergeometric-cdf";
   import hypergeometric_pmf from "@stdlib/stats-base-dists-hypergeometric-pmf";
+
+  import type HypergeometricParameters from "./HypergeometricParameters";
+  import OutputChart from "./OutputChart.svelte";
 
   /** Input parameters for the hypergeometric distribution calculation*/
   export let params: HypergeometricParameters;
-  $: p =
-    hypergeometric_pmf(
-      params.desired_success_count,
-      params.deck_size,
-      params.deck_success_count,
-      params.draw_count
-    ) +
-    (1.0 -
-      hypergeometric_cdf(
-        params.desired_success_count,
-        params.deck_size,
-        params.deck_success_count,
-        params.draw_count
-      ));
-  $: value = `${(100.0 * p).toFixed(2)}%`;
 
-  $: val_pmf = range(0, params.draw_count)
+  // Calculate the PMF for each possible value of k
+  $: value_pmf_list = range(0, params.draw_count)
     .map((k) => [
       k,
       hypergeometric_pmf(
@@ -34,26 +20,26 @@
     ])
     // Filter out near-zero probabilities
     .filter(([_, p]) => p > 1e-6);
-  $: max_pmf = val_pmf.reduce((max, [_, p]) => Math.max(max, p), 0.0);
-  $: val_pmf_scale = val_pmf.map(([k, p]) => [k, p, p / max_pmf]);
 
-  // Other utils
+  // Calculate the probability to draw at least params.desired_success_count in params.draw_count draws
+  // from a deck of params.deck_size cards that contains params.deck_success_count successes.
+  // One way to do this is to sum all PMF values greater than or equal to params.desired_success_count.
+  // Another way is to use the CDF function, but we already have all the PMF values calculate for the OutputChart.
+  $: answer = value_pmf_list
+    .filter(([value, _]) => value >= params.desired_success_count)
+    .reduce((sum, [_, pmf]) => sum + pmf, 0.0);
+
+  // Format the answer as a percentage with 2 decimal places
+  $: answer_as_pct = `${(100.0 * answer).toFixed(2)}%`;
+
+  /** Returns the pluralized form of a noun if count is not 1. */
   const pluralize = (count: number, noun: string, suffix = "s") =>
     `${noun}${count !== 1 ? suffix : ""}`;
 
+  /** Convenience function for returning an array of values between [start, end)*/
   const range = (start: number, end: number) => {
     const length = end - start + 1;
     return Array.from({ length }, (_, i) => start + i);
-  };
-
-  const label_font_size = (k: number) => {
-    if (k < 10) {
-      return "1.0em";
-    } else if (k < 20) {
-      return "0.75em";
-    } else {
-      return "0.5em";
-    }
   };
 </script>
 
@@ -74,38 +60,20 @@
 -->
 <div class="output-container">
   <!-- prettier-ignore -->
-  <p class="human-result">
+  <p class="answer-context">
     Probability to draw at least <span class="emphasize">{params.desired_success_count}</span>
     {pluralize(params.desired_success_count, "success", "es")} in <span class="emphasize">{params.draw_count}</span> {pluralize(params.draw_count, "draw", "s")} 
     from a deck of <span class="emphasize">{params.deck_size}</span>
     {pluralize(params.deck_size, "card", "s")} that contains <span class="emphasize">{params.deck_success_count}</span>
     {pluralize(params.deck_success_count, "success", "es")}
   </p>
-  <p class="raw-result">{value}</p>
-  <table
-    class="charts-css column show-labels hide-data show-primary-axis data-spacing-3"
-    style={`font-size: ${label_font_size(val_pmf_scale.length)};`}
-  >
-    <caption>Hypergeometric Probability Distribution</caption>
-    <thead><tr><th>Successes (k)</th> <th>Probability</th></tr></thead>
-    <tbody>
-      {#each val_pmf_scale as [k, pmf, scale], i}
-        <tr>
-          <th scope="row">{k}</th>
-          <td
-            style={`--size: ${scale}; --color: ${
-              k >= params.desired_success_count
-                ? "orange"
-                : "rgba(100,108,255, 0.5) "
-            }`}
-          >
-            <span class="data">{pmf}</span>
-            <span class="tooltip">{(100.0 * pmf).toFixed(2)}%</span>
-          </td>
-        </tr>
-      {/each}
-    </tbody>
-  </table>
+
+  <p class="answer-as-pct">{answer_as_pct}</p>
+
+  <OutputChart
+    {value_pmf_list}
+    desired_success_count={params.desired_success_count}
+  />
 </div>
 
 <style>
@@ -113,23 +81,15 @@
     font-weight: bold;
     font-size: 1.2em;
   }
-  .human-result {
+  .answer-context {
     font-size: 1.1em;
     text-align: center;
   }
-  .raw-result {
+  .answer-as-pct {
     font-size: 4em;
     font-weight: bold;
     line-height: 0.1em;
     color: orange;
     margin: 0.75em;
-  }
-  table {
-    width: 100%;
-    height: 200px;
-    max-width: 100%;
-    max-height: 200px;
-    overflow-x: hidden;
-    overflow-y: hidden;
   }
 </style>
---
+++ src/lib/OutputChart.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+  import "charts.css";
+
+  // A List of [value, pmf] pairs
+  export let value_pmf_list: number[][];
+  export let desired_success_count: number;
+
+  $: max_pmf = value_pmf_list.reduce((max, [_, p]) => Math.max(max, p), 0.0);
+
+  /** The chart label font size is a function of the number of columns in the chart*/
+  const label_font_size = (column_count: number) => {
+    if (column_count < 10) {
+      return "1.0em";
+    } else if (column_count < 20) {
+      return "0.75em";
+    } else {
+      return "0.5em";
+    }
+  };
+</script>
+
+<!--
+    @component
+    OutputChart Component
+  -->
+<table
+  class="charts-css column show-labels hide-data show-primary-axis data-spacing-3"
+  style={`font-size: ${label_font_size(value_pmf_list.length)};`}
+>
+  <caption>Hypergeometric Probability Distribution</caption>
+  <thead><tr><th>Successes (k)</th> <th>Probability</th></tr></thead>
+  <tbody>
+    {#each value_pmf_list as [value, pmf], i}
+      <tr>
+        <th scope="row">{value}</th>
+        <td
+          style={`--size: ${pmf / max_pmf}; --color: ${
+            // TODO(jshrake): Hardcoded colors, need to define in app.css instead
+            value >= desired_success_count
+              ? "orange"
+              : "rgba(100,108,255, 0.5) "
+          }`}
+        >
+          <span class="data">{pmf}</span>
+          <span class="tooltip">{(100.0 * pmf).toFixed(2)}%</span>
+        </td>
+      </tr>
+    {/each}
+  </tbody>
+</table>
+
+<style>
+  table {
+    width: 100%;
+    height: 200px;
+    max-width: 100%;
+    max-height: 200px;
+    overflow-x: hidden;
+    overflow-y: hidden;
+  }
+</style>
