008301540373924e4929b67ffad3e11069a348b3
Justin Shrake <justinshrake@gmail.com>
2023-01-16 19:13:31 -0800

    Satisfy minimal PWA requirements

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,46 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     svelte(),
-    VitePWA({ registerType: 'autoUpdate' })
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicon.ico",
+        "favicon-16x16.png",
+        "favicon-32x32.png",
+        "apple-touch-icon.png",
+        "safari-pinned-tab.svg",
+      ],
+      manifest: {
+        name: "Card Game Calculator",
+        short_name: "card-game-calculator",
+        description:
+          "Calculate the probability to draw cards using the hypergeometric distribution.",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+      },
+    }),
   ],
-})
+});
